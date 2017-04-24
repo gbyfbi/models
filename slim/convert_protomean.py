e273e3156d842509026b34e661f6ac6dd37cfe9e
GBY <gaoboyang@gmail.com>
2017-04-24 18:59:22 +0200

    rename the input file

--- slim/convert_protomean.py
+++ slim/convert_protomean.py
@@ -7,7 +7,7 @@ from caffe.proto.caffe_pb2 import BlobProto
 #     print ("Usage: python convert_protomean.py proto.mean out.npy")
 #     sys.exit()
 
-proto_mean_file_path = '/home/gao/Downloads/finetune_alexnet_with_tensorflow_gby/caffe_ilsvrc12/imagenet_mean.binaryproto'
+proto_mean_file_path = 'preprocessing/imagenet_mean.binaryproto'
 numpy_mean_file_path = 'preprocessing/imagenet_mean.npy'
 blob = BlobProto()
 data = open(proto_mean_file_path, 'rb').read()
